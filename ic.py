2206ccc0ab6aa2734500856de10c4b62b8502c63
ankhchen <41895839+ankhchen@users.noreply.github.com>
2019-08-22 16:43:22 +0800

    Update ic.py

--- ic.py
+++ ic.py
@@ -2,8 +2,7 @@ from selenium import webdriver
 import time
 
 # cookie
-c = 'X_APPLE_WEB_KB-IPNFN3A66RFT6IBPUBL7LRVXPL0="v=1:t=IAAAAAAABLwIAAAAAF1BQ9oRDmdzLmljbG91ZC5hdXRovQCPrJ80TfnLHyHpxoWjjmKZ6GIrwA9yVErOwIy4Kc2XkNF78vDRdsOBI52ntUG6mV4xgc-l5USLKXthzOwo3X8_1z-U4KjAh9-IrgElvRAgEq-ZWF-QpiJRcXcVjlA6It3GzvCo5_0wKgC1q8ANm1tspWQgDg~~"; X-APPLE-WEBAUTH-HSA-TRUST=d6d4b4d71efaf7148cdd34f3669fe051fb9cc22d2c517f1ddef7d3b6e1a3a0dc_HSARMTKNSRVXWFlaeoGEDM0nb9CBQwGoIlY09kqugKCK9ZZwGwe9a80frz7R+dBiF3H5xqX5vc1yOmC2KBZpnTrWr7qxvYMmyBqQAup+EQGWfRkznEhAN7MOU8SAqKbHSa+Rj22KS2WrSRVX; wmsid=85; X-APPLE-WEBAUTH-PCS-Documents="TGlzdEFwcGw6MTpBcHBsOjE6AX/yKqZ9xCAbMdAlunB8n/1+ziRJFi0UlxZXJQXAbi0DlhWkeTz3mgK6yAYDhHGjOYvTobEuv7wsegxyf/Jt2QUtRZdZDp8kTPwMi03L3hi8zIdkCI81WsZXo/IH2+5FY+W+RC+uUIOJpcBOncb3uxF8eGv6At13FvhPpaRMlZWd9ewIZQ8P9w=="; X-APPLE-WEBAUTH-PCS-Photos="TGlzdEFwcGw6MTpBcHBsOjE6ATNsGbcijJ+vRZkiZfX92VwInoug2RBf9oIQHmWQvqDBlChAg8dXNSdFeGxM1lw7CawHNwR0l64HQYnvUZtcbDzEGju33CXYuLQqkndIMKKt6olVVsCmW9swT4V0LZjns3W5NW/V3ZC1heAX1SqlFNsbiArch09JE/geIJi5XzVrQEbweW9log=="; X-APPLE-WEBAUTH-PCS-Cloudkit="TGlzdEFwcGw6MTpBcHBsOjE6AXMfolQXYYecaG2Dfcaf/+oOX2ydcFa0cbcL2xre6NcPsBC5bsXKP7PChYM5oNpUK7k+U7V4P1IgI61X/wgbsyww3xzsmmL9c3jzOAncr3QjkFA6tOp1YERKeFKR6wj0+XuwBnwrPjUZTIQHcdsGKuki3bSmDHGt3LtimJ4C8qa31YS0ZojQ2w=="; X-APPLE-WEBAUTH-PCS-Safari="TGlzdEFwcGw6MTpBcHBsOjE6AQRo82MMVG8/GS/UHbpUFuQvwrn9oZEevB1mzNXeDIpuKhTKbd8McMcWJzTH7lSbXMNRly+JM4ngJo4oySk10KAAal+icAd4Kwgd9spQXtab5o6nmA6RTSajsec8j2AZHdzgA5K5yw7HeieGEJOfF7OCVOTNOgRQ3IcIDMJLTPS9w5dK30vuRg=="; X-APPLE-WEBAUTH-PCS-Mail="TGlzdEFwcGw6MTpBcHBsOjE6AV6RYPQh46ZSRf8BV+Y1hJAb2wf7GXLtYLlLD35gg0T49YqzyrVeAenn5bhRfChfzcuiPt7ycvn0MZhPkywYlWDKpiXe580Az9i5UoKbhKLAueWzgqphR2qhQoLfwOy07+pVlf1uaWHEfalni0qtYofY21zHXg16qj8sqj7VcxBHlZ/o3+bzEw=="; X-APPLE-WEBAUTH-PCS-Notes="TGlzdEFwcGw6MTpBcHBsOjE6Aenb7AI3yQ8C8aYl/v4quKYU7Xt8QKdvspoMNUObMdT2SkczDRBaN41vqN9oxj9/4UJKHhUMXcx2vBDiqosevsjtt8kbtpM2e5S6vGIOTwaYwWiDxSVg6rtKe04RQf7Exe5Jxcfm5EkBaXymp/kwD9vaGavbKd4zC+aETn0kdlwwQSNMR7RAjQ=="; X-APPLE-WEBAUTH-PCS-News="TGlzdEFwcGw6MTpBcHBsOjE6AcfEtIwjUDoI9LLUpWBBN32oiEo/O1jsPDS6DKwAt4TMrrreF7EzMiV7jQ3zFaZxmzNOF2Z8iuywe14KSTFzH3xXdOxrZlvr8RcVPQf0MhwVv3bqA/IPvrjDPL2jLAkeyiE5VgxXofr4pwhzT+OWQaAlCq96Llbw3eC+g987xoTZHr2gXypbNQ=="; X-APPLE-WEBAUTH-PCS-Sharing="TGlzdEFwcGw6MTpBcHBsOjE6AUzC3zL4ZLHPa9vCWn86TcENjKm0AKvivKcc0L2yV3oF75lLRrE0wo7p96Elw+VmEHpo9De0lMBIZiVcbovk/nGqW8iJyDSIcnEXVsUQt5EMzWVUbX5oOKj/+V8mrDBhtKZbxXYN1qYLeiWq7EGOr0iNKgFBPQUJx8Sjz3TD3RL5rtOKRCsmQg=="; X-APPLE-WEBAUTH-LOGIN="v=1:t=IAAAAAAABLwIAAAAAF1WTmMRDmdzLmljbG91ZC5hdXRovQDP8leI2Y9wqqLp2H588qHcYugxfXHwOtyIhVTA8PmepxPUocLbmakuCti9KXKjc--IfgFZmCxK-_Z6rFtcaDCz7N1PM6IpuWgkIgTQvCgtwnLGPdXK1dpRu71w2LCEfzyrHcqOrpFDR5aqoJHRXf_a8lKelA~~"; X-APPLE-WEBAUTH-USER="v=1:s=0:d=16763304089"; X-Apple-GCBD-Cookie=1; X-APPLE-WEB-ID=F8D2B6E59738B4BD8560BC726695E7F1985407B1; X-APPLE-WEBAUTH-VALIDATE="v=1:t=IAAAAAAABLwIAAAAAF1WX8QRDmdzLmljbG91ZC5hdXRovQBo-qoVXcq-uUou4H6pOW4YDSAgZ0F5FxWpfqK6-duBcDWKspyPXpMkqus1Iad6PyshlLcAvBd8qo5sEXjsa3Fgt94wEvcVMZ7BbYcxkXKfcBfEF91SG3XPyEIAY0CWPPT8DacnZAtWVJFRcEKvI9Co7Rqt-A~~"; X-APPLE-WEBAUTH-TOKEN="v=2:t=IAAAAAAABLwIAAAAAF1WX8gRDmdzLmljbG91ZC5hdXRovQAB-PQdaceEpo7oL9TvBSTHAupUshppDaces7dM6VDEDZHMftroRtAYPEZe_06VjrTLnnco2X1trlVe8tIMz-wVuFgbGRz7_oUrX_EmdRWbXGReEN8LqdGbWPdZUX5iP4JSCbWQ_PuBT2ce-JaDCoedIH6mHA~~"'
-
+c = 'X_APPLE_WEB_KB-IPNFN3A66RFT6IBPUBL7LRVXPL0="v=1:t=IAAAAAAABLwIAAAAAF1BQ9oRDmdzLmljbG91ZC5hdXRovQCPrJ80TfnLHyHpxoWjjmKZ6GIrwA9yVErOwIy4Kc2XkNF78vDRdsOBI52ntUG6mV4xgc-l5USLKXthzOwo3X8_1z-U4KjAh9-IrgElvRAgEq-ZWF-QpiJRcXcVjlA6It3GzvCo5_0wKgC1q8ANm1tspWQgDg~~"; X-APPLE-WEBAUTH-HSA-TRUST=d6d4b4d71efaf7148cdd34f3669fe051fb9cc22d2c517f1ddef7d3b6e1a3a0dc_HSARMTKNSRVXWFlaeoGEDM0nb9CBQwGoIlY09kqugKCK9ZZwGwe9a80frz7R+dBiF3H5xqX5vc1yOmC2KBZpnTrWr7qxvYMmyBqQAup+EQGWfRkznEhAN7MOU8SAqKbHSa+Rj22KS2WrSRVX; wmsid=85; X-APPLE-WEBAUTH-PCS-Documents="TGlzdEFwcGw6MTpBcHBsOjE6AcpQzVgQfmOHhWEzepszIk0Ed/IldR/XzuyScjNthoOTXIp47krMaJ/CQaKzpnjnXn1CbIX3TYEtkuARoGsREGLdTd1T+0XEwKT/h85dKjoOhGypwgAQsuNYt3h7xvbQ/LSZAvV0cGOgE92x5tU4xAmoddrUW75dxST92AKxShGo7bXx05aheQ=="; X-APPLE-WEBAUTH-PCS-Photos="TGlzdEFwcGw6MTpBcHBsOjE6AUIBPxFda0ZHgc2cc4wrmALT8HhVtpGFdxoE6BuEqt6QjOBOxchj7la6Rzbv8X6V//Hyp8xLy6gYPQyJFaxlJLifOw+P8Ln2Zrw65xmXqioCNS1aNXwdYMpCAa7MrhUVXTgWu01Vu6/29R+j09YiRrA2kEwRhNiLe75wpQfPSl4w14o7+tJRmQ=="; X-APPLE-WEBAUTH-PCS-Cloudkit="TGlzdEFwcGw6MTpBcHBsOjE6AfKeaAKELvFXCzJq2rGJ51cnz8e3SM4IgVFvi5ZKDWeSV1RRNKanz1KmBR0I2SkEaKG3C3bjs8heoAbSbFON/UFeTKaG/hed3Cn6k7TM8AW4wgE3d9HC1xRlLLhPaZryDdBlF4GCqcmULPkYGiGkvwstmxpy1FX+JjnKDLDENHzHuF38sUzYdg=="; X-APPLE-WEBAUTH-PCS-Safari="TGlzdEFwcGw6MTpBcHBsOjE6AcgiRM67kZ7Zq7S047jtQCnfFCL+E4nX9bvx6cVHFK8FT+C3FsZeh9vNlkpX6YEBVp+FTqAFHo6/eoCU4ZWk50BRN/hOHcrQA4nRk+pvQ1B5KkXw+t3pDzX+xIwo2NV844BR9IJM/P+zUndyv7fAijXDmmVn1HR5yXlHOd6E1yeXlPq8NaPASg=="; X-APPLE-WEBAUTH-PCS-Mail="TGlzdEFwcGw6MTpBcHBsOjE6AVf76LMcqru5+9d9Q7fyWjCFyNGB88wO30+xaPHLrKV1pgA9BhijR9YzyZkyrJxLla5XIlQ2IIMruirXdMpGwBJ5Q0viI9u7byvU7jDLpozNmSUV787tkMnyhzH33I+GTkDmCV2BsI+WsrbzoSmcDhBEsxOtw2M8g3ESCAFPCxo5VUUbqah7vQ=="; X-APPLE-WEBAUTH-PCS-Notes="TGlzdEFwcGw6MTpBcHBsOjE6AdkpK/NapcGtP2vcSVWB6I5oZGUA+spdS37RkozWkdHmixbCDAlM3uaLf5ZyIh6pKLd8Kn8qPFK+kBmGwcKd18bfFzkvOMUKkXwHrUGaPCNk5UZmWrSl04oL/fotQx1VCey+/0jrRwEQVDOhZbcJnpeHkX844+zeWRU8SvZg64eXAjDaVn4VmA=="; X-APPLE-WEBAUTH-PCS-News="TGlzdEFwcGw6MTpBcHBsOjE6AR9mLP7hvMvXEXy8JSSRIfiqBTWuilBye4bdlaKo6hElzruAdLxX9zdL3ovv4X7Qc9FBzlvhrWz1OSQPqvI71MSnn2rzHA3QndNTa+hLCYtHBLIb7eCwaZS4vIEkTyqgcVDlNYegkeO6sg1USPhq+3H/bys8YsIaEsY5zrASz9Cqet6nbs6/tQ=="; X-APPLE-WEBAUTH-PCS-Sharing="TGlzdEFwcGw6MTpBcHBsOjE6AWn1nb6JgxKo5SDRHmyMJmrgm3jrYmGD50qI7Tk9ISnxSnIsBtlQ63MXB1QnVn01PUQGu6xO7k8rr97rru/0mL8+aHMjh0b48avl6/CmKn79Zb8DO5r19du0PbhLTzc/IjAplLfNe5Ke8vG0qufLj2G3aa8UL+x2RuFEy0HDbxraLFMeI5fWQg=="; X-APPLE-WEBAUTH-LOGIN="v=1:t=IAAAAAAABLwIAAAAAF1eT3ERDmdzLmljbG91ZC5hdXRovQBGMeTdlpY_ODL5C0QaVkDoBgiZo48iHFCCJxEeWbSsk9a4F7jkfwMKNfd_xeOLv4zbhlMsjFrXmXJ66RDAI5s1c5H1XBkV306OPK6QyeTzXM8SLjMagbIuI-maKXFHZ1L5HJA7SVlJDL5jC9qi5n5pkNCbZQ~~"; X-APPLE-WEBAUTH-VALIDATE="v=1:t=IAAAAAAABLwIAAAAAF1eT3ERDmdzLmljbG91ZC5hdXRovQBGMeTdlpY_ODL5C0QaVkDoBgiZo48iHFCCJxEeWbSsk9a4F7jkfwMKNfd_xeOLv4zbhlMsjFrXmXJ66RDAI5s1c5H1XBkV306OPK6QyeTzXIJutbpDUvvNILvUHFDQNenLjDYApRbRQ_9zXlvdc3REzz7AlQ~~"; X-APPLE-WEBAUTH-USER="v=1:s=0:d=16763304089"; X-Apple-GCBD-Cookie=1; X-APPLE-WEB-ID=F8D2B6E59738B4BD8560BC726695E7F1985407B1; X-APPLE-WEBAUTH-TOKEN="v=2:t=IAAAAAAABLwIAAAAAF1eT3URDmdzLmljbG91ZC5hdXRovQBAO16gYrOqKWswXEryE8T41zDhMlYuET9z7RJYb4BRMkJhgsLLAiiuOJFu0AaKpSfwLJBaGfip3njg-oNmFATXWRJkhmDSrc2uylNMpGCDZzQ7HHzaQvX-qVFgG3jY38bFrPhBGha9try_8o6DWw1JkhqL8Q~~"'
 
 def upload():
     url = 'https://www.icloud.com/#mail'
@@ -22,20 +21,31 @@ def upload():
             HostOnly=False,
             Secure=False
         ))
-    time.sleep(6)
+    time.sleep(10)
     driver.get(url)
     print(driver.title)
     driver.switch_to.frame(driver.find_element_by_tag_name('iframe'))
-    time.sleep(4)
+    time.sleep(10)
     driver.find_element_by_class_name('add-button').click()
-    time.sleep(5)
+    time.sleep(10)
+    # 新建文件夹
     driver.find_element_by_id('toolbar-actions-menu').click()
-    time.sleep(2)
+    time.sleep(10)
+    # driver.find_element_by_class_name('me-sourcelist-row-title').send_keys('重要的文件')
+    # time.sleep(2)
     # driver.find_element_by_class_name('value ellipsis').click()
-    driver.find_element_by_link_text('规则')
+    # 设置规则
+    driver.find_element_by_xpath('//*[@id="actions-rules"]').click()
+    time.sleep(10)
+    driver.find_element_by_xpath('//*[@class="sc-button-label sc-regular-size"]').click()
+    time.sleep(6)
+    driver.find_element_by_class_name('dropdown atv4 sc-view sc-button-view criteria-button button sc-large-size').click()
+
+    # driver.find_element_by_xpath('//*[@class="field"]').send_keys('742517254@qq.com')
     print('===')
     time.sleep(10)
 
 
 if __name__ == '__main__':
     upload()
+
